acce2eecbd2f4c90d5a364ca10da687304a430ee
Digvijay7310 <kumardigvijay377@gmail.com>
2025-08-11 17:27:56 +0530

    create some components and home page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React from "react"
+import {Route, Routes} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage"
+import ProfilePage from "./pages/ProfilePage"
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="bg-[url('./src/assets/bg_image.jpg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/assets/assests.js
@@ -0,0 +1,115 @@
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import user1 from './user1.jpg'
+import user2 from './user2.jpg'
+import user3 from './user3.jpg'
+import user4 from './user4.jpg'
+import user5 from './user5.jpg'
+import bg_image from './bg_image.jpg'
+import logo from './logo.jpg'
+import search_icon from './search_icon.jpg'
+import avatar_icon from './avatar_icon.jpg'
+import menu_icon from './menu_icon.jpg'
+import arrow_icon from './arrow_icon.jpg'
+import help_icon from './help_icon.jpg'
+
+const assets = {
+    img1, img2, img3, user1, user2, user3, user4, user5, bg_image, logo
+    ,search_icon, avatar_icon, menu_icon, arrow_icon, help_icon
+}
+export default assets;
+
+export const imagesDummyData = [user1, user2, user3, user4, user5]
+
+export const userDummyData = [
+    {
+        "_id": "384875f849rj49943",
+        "email": "test1@gmail.com",
+        "fullName": "Alison Martin",
+        "profilePic": user1,
+        "bio": "Hi Everyone, I am using QuickChat"
+    }, 
+    {
+        "_id": "384875f849rj49944",
+        "email": "test2@gmail.com",
+        "fullName": "jimmy ",
+        "profilePic": user2,
+        "bio": "Hi Everyone, I am using QuickChat"
+    }, 
+    {
+        "_id": "384875f849rj49945",
+        "email": "test3@gmail.com",
+        "fullName": "Noel",
+        "profilePic": user3,
+        "bio": "Hi Everyone, I am using QuickChat"
+    }, 
+    {
+        "_id": "384875f849rj49946",
+        "email": "test4@gmail.com",
+        "fullName": "Richard",
+        "profilePic": user4,
+        "bio": "Hi Everyone, I am using QuickChat"
+    }, 
+    {
+        "_id": "384875f849rj49947",
+        "email": "test5@gmail.com",
+        "fullName": "Peter",
+        "profilePic": user5,
+        "bio": "Hi Everyone, I am using QuickChat"
+    }, 
+]
+
+export const messagesDummyData = [
+    {
+        "_id": "jh83498ujkd8uu994ru8r4234",
+        "senderId": "384875f849rj49947",
+        "receiverId": "384875f849rj49946",
+        "text": "hello how are you this is first chat",
+        "seen": "true",
+        "createdAt": "2025-04-28T10:23:27.844Z",
+    },
+    {
+        "_id": "jh83498ujkd8uu994ru8r4235",
+        "senderId": "384875f849rj49946",
+        "receiverId": "384875f849rj49947",
+        "text": "Yes bro me too chat",
+        "seen": "true",
+        "createdAt": "2025-04-28T10:24:27.844Z",
+    },
+    {
+        "_id": "jh83498ujkd8uu994ru8r4236",
+        "senderId": "384875f849rj49947",
+        "receiverId": "384875f849rj49946",
+        "text": "How is Your day now",
+        "seen": "true",
+        // "image": img1,
+        "createdAt": "2025-04-28T10:25:27.844Z",
+    },
+    {
+        "_id": "jh83498ujkd8uu994ru8r4237",
+        "senderId": "384875f849rj49946",
+        "receiverId": "384875f849rj49947",
+        "text": "Lorem",
+        "seen": "true",
+        // "image": img2,
+        "createdAt": "2025-04-28T10:26:27.844Z",
+    },
+    {
+        "_id": "jh83498ujkd8uu994ru8r4238",
+        "senderId": "384875f849rj49947",
+        "receiverId": "384875f849rj49946",
+        "text": "jhkjh",
+        "seen": "true",
+        // "image": img3,
+        "createdAt": "2025-04-28T10:27:27.844Z",
+    },
+    {
+        "_id": "jh83498ujkd8uu994ru8r4239",
+        "senderId": "384875f849rj49946",
+        "receiverId": "384875f849rj49947",
+        "text": "Yes bro me kjkj chat",
+        "seen": "true",
+        "createdAt": "2025-04-28T10:23:27.844Z",
+    }
+]
